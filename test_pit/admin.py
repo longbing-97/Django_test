c4ee6cf047601f799f352c18bf1a7cd09db03195
binglong <binglong@whale.im>
2023-04-05 19:08:49 +0800

    增加ProjectAdmin类，对列表页面经行配置；

--- test_pit/admin.py
+++ test_pit/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Project
+from django.contrib.admin import ModelAdmin
+
+
+# 第一步创建ModelAdmin的继承类
+class ProjectAdmin(ModelAdmin):
+    # 第二步：指定要显示的字段
+    list_display = ['id', 'name', 'version', 'type', 'status', 'created_by', 'created_at']
+    list_display_links = ['id', 'name']
+    # 第三步：指定可过滤的列
+    list_filter = ['created_at', 'type']
+    # 第四步：指定可查询的列
+    search_fields = ['name']
+
+
 
 # Register your models here.
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 
 admin.site.site_header = '自动化测试平台后台管理'
 admin.site.site_title = '测试平台后台'
